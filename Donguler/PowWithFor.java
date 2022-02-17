08e901f4cb05f253707bc023c716ef60cdefe12a
dikkulah <dikkulah@gmail.com>
2022-02-18 01:46:45 +0300

    for ile üs alma

---
+++ Donguler/PowWithFor.java
@@ -0,0 +1,21 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class PowWithFor {
+    public static void main(String[] args) {
+        int number, pow, result = 1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Üssü alınacak sayı: ");
+        number = scanner.nextInt();
+        System.out.print("Üs : ");
+        pow = scanner.nextInt();
+
+
+        for (int i = 1; i <= pow; i++) {
+            result *= number;
+        }
+        System.out.println(result);
+    }
+}
