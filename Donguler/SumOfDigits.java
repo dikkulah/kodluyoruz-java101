da0489e098f17cd241c21e088b3a8a72c7725cf8
dikkulah <dikkulah@gmail.com>
2022-02-18 17:15:49 +0300

    BASAMAK TOPLAMA

--- Donguler/SumOfDigits.java
+++ Donguler/SumOfDigits.java
@@ -8,21 +8,14 @@ public class SumOfDigits {
         Scanner input = new Scanner(System.in);
         System.out.print("Sayı Giriniz :");
         int number = input.nextInt();
-        int basNumber = 0;
-        int tempNumber = number;
         int basValue;
         int result = 0;
-        // BASAMAK SAYISI BULMA
-        while (tempNumber != 0) {
-            tempNumber /= 10;
-            basNumber++;
-        }
-        // HER BASAMAĞI TOPLAMA
-        tempNumber = number;
-        for (int i = 1; i <= basNumber; i++) {
-            basValue = tempNumber % 10;
+
+        while(number!=0) {
+            basValue = number % 10;
             result += basValue;
-            tempNumber /= 10;
+            number /= 10;
+
         }
         System.out.println(result);
 
