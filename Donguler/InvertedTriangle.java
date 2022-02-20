972cfa5c36b0d25523b7e2bba637ff2fee274b4d
dikkulah <dikkulah@gmail.com>
2022-02-20 04:19:02 +0300

    Ters üçgen

---
+++ Donguler/InvertedTriangle.java
@@ -0,0 +1,22 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class InvertedTriangle {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        n = scanner.nextInt();
+
+        for (int i = n ; i > 0; i--) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2*i-1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
