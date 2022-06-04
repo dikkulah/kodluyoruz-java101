6ccb2e2cbaca3b44513bff6306bbb9ee5540777e
dikkulah <dikkulah@gmail.com>
2022-06-04 23:28:20 +0300

    Dizide tekrar eden çift sayıları bulan program - Java101 Ödevi

---
+++ Arrays/ArraySort.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dizi boyutu giriniz: ");
+        int[] arr = new int[scanner.nextInt()];
+
+        System.out.println("Dizinin elemanlarını giriniz: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Dizinin " + (i + 1) + ". elemanı: ");
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.print("Sıralama: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
