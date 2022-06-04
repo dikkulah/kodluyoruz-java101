2e8d0bc29448edb28d823ec99f21bc6f88392858
dikkulah <dikkulah@gmail.com>
2022-06-04 21:29:08 +0300

    Listedeki sayıların harmonik serilerinin toplamı.

--- Arrays/HarmonicSum.java
+++ Arrays/HarmonicSum.java
@@ -5,21 +5,13 @@ public class HarmonicSum {
         int[] list = {15, 1, 7, 11, 2};
 
 
-        double result = 0;
         double sum = 0;
         System.out.print("Sayı giriniz: ");
 
         for (int eleman : list) {
-            for (int i = 1; i <= eleman; i++) {
-                result += (1d / i);
-                System.out.println("Harmonik serinin " + i + ". değeri " + 1d + "/" + i);
-            }
-            System.out.println(eleman + " sayısının Harmonik Toplamı : " + result);
-            sum += result;
-            result = 0;
-
+            sum += 1d / eleman;
         }
-        System.out.println("Harmonik serilerin ortalaması: "+ sum/ list.length);
+        System.out.println("Harmonik  ortalama: " + list.length / sum);
 
     }
 }
